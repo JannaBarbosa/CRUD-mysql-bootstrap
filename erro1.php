0eeccbcc780b862afcdf4f613bd8aab28834660b
Janna <janny_b@hotmail.com>
2020-12-21 18:21:18 -0300

    Alteração na validação do login
    Alteração no arquivo index1.php para aperfeiçoar a validação durante o login do usuário

--- erro1.php
+++ erro1.php
@@ -1,17 +1,18 @@
 
 
+
+
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <div class="container"style="width: 500px;margin-top: 20px">
 	<center>
 		<h3>Erro!</h3>
-	<h4>Você não possui cadastro ou o usuário informado está incorreto</h4>
+	<h4> Senha incorreta.<br> 
+	Favor digitar a senha correta!</h4>
  	</center>
 <div style="padding-top: 20px">
 	<center>
-	<a href="cadastro_usuario_externo.php" role="button" class="btn btn-sm btn-primary">Efetuar cadastro</a>
-	</center>
-	<center style="padding-top: 10px;">
-	<a href="index.php" role="button" class="btn btn-sm btn-primary" >Tentar novamente realizar o login</a>
+	<a href="index.php" role="button" class="btn btn-sm btn-primary">Voltar</a>
 	</center>
 </div>
 
--- index1.php
+++ index1.php
@@ -2,37 +2,38 @@
 include 'conexao.php';
 include 'script/password.php';
 
-
 $usuario = $_POST['usuario'];
 $senha1 = $_POST['senha'];
 
-
-
 $sql = "SELECT  `emailusuario`,`senhausuario` FROM `usuarios` WHERE emailusuario = '$usuario' and status='Ativo'";
 $buscar = mysqli_query($conexao,$sql);	
+
  $total = mysqli_num_rows($buscar);
+ #echo $total;
 
- while ($array = mysqli_fetch_array($buscar)) {
+ if($total==0){header("Location:erro.php"); }
+ else {
+
+ 	while ($array = mysqli_fetch_array($buscar)) {
+ 	$usuariocadastrado = $array['emailusuario'];
  	$senha = $array['senhausuario'];
+    $senhadecodificada = sha1($senha1);
 
- 	$senhadecodificada = sha1($senha1);
+    if(($usuario == $usuariocadastrado)and($senhadecodificada == 
+ 		$senha)){
 
-
- if($total > 0){
-# Conferir a senha
- 	if($senhadecodificada == $senha){
- 		session_start();
+    	session_start();
  		$_SESSION['usuario'] = $usuario;
  		header("Location:menu.php");
- 	} else{
- 		header("Location:erro.php");
- }
- 	}
-  else { 
+  } else{
+  	header("Location:erro1.php");
+  }
 
- 	header("Location:erro.php");
- }
+
 
  }
+
+}
+ 
 
 ?>
